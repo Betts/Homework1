a48630df43593e03ef8d8b13aabc32d2e25957dd
Betts <84404748+Betts@users.noreply.github.com>
2022-01-27 18:08:50 -0500

    Add files via upload

---
+++ HW1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+
+
+public class HW1{
+    public static void main(String[] args) {
+        long i = 0;
+        long limit = (long) Math.pow(10, 8);
+
+
+
+    }
+
+}
