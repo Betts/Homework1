969dcdb9fcd8392d7ec589bbfb16dcd7d8bdd9e2
Betts <84404748+Betts@users.noreply.github.com>
2022-01-28 10:07:43 -0500

    isPrime method added

--- HW1.java
+++ HW1.java
@@ -14,4 +14,22 @@ public class HW1{
 
     }
 
+    // Method to check whether the incoming number is a prime or not. 
+    // Returns False if num is not a prime, returns True if num is a prime. 
+    public static boolean isPrime(int num)
+    {
+        if (num <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
 }
