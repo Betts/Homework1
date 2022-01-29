f70a4f0f1a6d3132134b6bd9a5a03e1be21508f6
Betts <84404748+Betts@users.noreply.github.com>
2022-01-28 20:19:33 -0500

    Now prints to file. Might need to change output formatting.

--- HW1.java
+++ HW1.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.concurrent.atomic.*;
+import java.util.stream.Collectors;
 import java.io.*;
 import java.lang.Math;
 import java.time.*;
@@ -9,28 +10,40 @@ import java.time.*;
 public class HW1{
     static AtomicInteger counter = new AtomicInteger(1);
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int i = 0;
         // Limit is 10^8, and it is the maximum value that our boss has requested
         int limit = 100000000;
-        // test
-        int[] primes = new int[limit + 1];
+
+        List<Integer> lst = new ArrayList<>();
+    
+        long sum = 0;
+        int totalPrimes = 0;
     
         long start = System.currentTimeMillis();
+
         while (i < limit)
         {
             i = counter.getAndIncrement();
             if (isPrime(i))
             {
-
-                System.out.println(i);
+                lst.add(i);
+                sum = sum + i;
+                totalPrimes = totalPrimes + 1;
             }
         }
         long end = System.currentTimeMillis();
-
-
-
-
+        List<Integer> lst1 = lst.stream().sorted(Comparator.reverseOrder()).limit(10).collect(Collectors.toList()); 
+        Collections.reverse(lst1);
+        long totalTime = end - start;
+        
+        File file = new File("primes.txt");
+        PrintStream stream = new PrintStream(file);
+        System.setOut(stream);
+        System.out.println("Total Time: " + totalTime +" ms.");
+        System.out.println("Total Primes Found: " + totalPrimes);
+        System.out.println("Sum of Primes: " + sum);
+        System.out.println("Top 10 primes: " + lst1);
     }
 
     // Method to check whether the incoming number is a prime or not. 
@@ -42,7 +55,9 @@ public class HW1{
             return false;
         }
 
-        for (int i = 2; i <= Math.sqrt(num); i++)
+        int sqNum = (int) Math.sqrt(num);
+
+        for (int i = 2; i <= sqNum; i++)
         {
             if (num % i == 0)
             {
